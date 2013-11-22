c77b982c8149d258dfc2de5de68b4b38db4752e4
robbiesmith <smith.rob@gmail.com>
2013-11-21 18:53:59 -0800

    more sockets in client

--- client/js/socket.js
+++ client/js/socket.js
@@ -2,7 +2,7 @@ var socket = io.connect("http://localhost:3000");
 console.log("socket is defined");
 var game = {};
 
-  	console.log("preparing for sockets");
+console.log("preparing for sockets");
     
 socket.on('game',function(data){
 console.log(data);
@@ -10,6 +10,11 @@ console.log(data);
 });
 
 
-    socket.emit('join', function(playerObj){
-    	console.log("emitted join", playerObj)
-    });
+socket.emit('join', function(playerObj){
+	console.log("emitted join", playerObj)
+});
+
+socket.on('alert', function(message) {
+	console.log(message);
+	alert(message);
+});
