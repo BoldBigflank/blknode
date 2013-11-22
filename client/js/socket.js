00207bfbee24fff71078a63b574e4f118a27e4f0
robbiesmith <smith.rob@gmail.com>
2013-11-21 18:42:53 -0800

    adding sockets support to client

---
+++ client/js/socket.js
@@ -0,0 +1,15 @@
+var socket = io.connect("http://localhost:3000");
+console.log("socket is defined");
+var game = {};
+
+  	console.log("preparing for sockets");
+    
+socket.on('game',function(data){
+console.log(data);
+//	data.board;
+});
+
+
+    socket.emit('join', function(playerObj){
+    	console.log("emitted join", playerObj)
+    });
